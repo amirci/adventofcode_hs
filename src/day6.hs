536936a9dcea75df00d019d7da333a79a89d2d31
Amir Barylko <amir@barylko.com>
2016-01-02 20:22:20 -0600

    Added day 6 part 2

--- src/day6.hs
+++ src/day6.hs
@@ -4,23 +4,52 @@ import Data.List
 import Data.Maybe
 import Debug.Trace
 import qualified Data.Text as T
-import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 type Light = (Int, Int)
-type LightBoard = Set.Set Light
+type Brightness = Int
 
+type LightBoard = Map.Map Light Brightness
+type Range = [Light]
+
+data Command = TurnOn | TurnOff | Toggle deriving (Show)
+
+mkLight :: (Int, Int) -> Light
+mkLight (a, b) = (a, b)
+
+mkBoard :: LightBoard
+mkBoard = Map.empty
 
 turnOn :: Light -> LightBoard -> LightBoard
-turnOn = Set.insert
+turnOn light board = Map.insert light 0 board
+
+allOn :: LightBoard -> Range -> Bool
+allOn board range = and $ map (flip isOn $ board) range
+
+allOff :: LightBoard -> Range -> Bool
+allOff board range = and $ map (flip isOff $ board) range
 
 turnOff :: Light -> LightBoard -> LightBoard
-turnOff = Set.delete
+turnOff = Map.delete
 
 lightCount :: LightBoard -> Int
-lightCount = Set.size
+lightCount = Map.size
+
+incBright :: Brightness -> Light -> LightBoard -> LightBoard
+incBright amount light = Map.insertWith (+) light amount
+
+decBright :: Int -> Light -> LightBoard -> LightBoard
+decBright amount = Map.adjust decrease
+  where decrease = (max 0) . (\a -> a - amount)
+
+brightQ :: Light -> LightBoard -> Brightness
+brightQ light = Map.findWithDefault 0 light
+
+brightness :: LightBoard -> Brightness
+brightness = Map.fold (+) 0
 
 isOn :: Light -> LightBoard -> Bool
-isOn = Set.member
+isOn = Map.member
 
 isOff :: Light -> LightBoard -> Bool
 isOff light board = not $ isOn light board
@@ -29,16 +58,32 @@ toggle :: Light -> LightBoard -> LightBoard
 toggle light board = op light board
   where op = if (isOn light board) then turnOff else turnOn
 
-command :: String -> LightBoard -> LightBoard
-command cmd board = foldr fn board coords 
+lightIt :: String -> LightBoard -> LightBoard
+lightIt instruction board = foldr (parseFn cmd) board coords 
+  where
+    (cmd, coords) = parse instruction
+    parseFn TurnOn = turnOn
+    parseFn TurnOff = turnOff
+    parseFn Toggle = toggle
+
+brightIt :: LightBoard -> String -> LightBoard
+brightIt board instruction = foldr (parseFn cmd) board coords 
+  where
+    (cmd, coords) = parse instruction
+    parseFn TurnOn  = incBright 1
+    parseFn TurnOff = decBright 1
+    parseFn Toggle  = incBright 2
+
+parse :: String -> (Command, [Light])
+parse instruction = (cmd, coords)
   where
     coords = [(i, j) | i <- [x..z], j <- [y..w]]  
-    (fn, (x:y:z:w:[])) = mapCoord $ parse words
-    mapCoord (fn, begin, end) = (fn, coord begin ++ coord end)
-    parse ("turn":"on":start:_:end:[])  = (turnOn, start, end)
-    parse ("turn":"off":start:_:end:[]) = (turnOff, start, end)
-    parse ("toggle":start:_:end:[])     = (toggle, start, end)
-    words = map T.unpack $ T.splitOn (T.pack " ") $ T.pack cmd
+    (cmd, (x:y:z:w:[])) = mapCoord $ parse' words
+    mapCoord (cmd, begin, end) = (cmd, coord begin ++ coord end)
+    parse' ("turn":"on":start:_:end:[])  = (TurnOn, start, end)
+    parse' ("turn":"off":start:_:end:[]) = (TurnOff, start, end)
+    parse' ("toggle":start:_:end:[])     = (Toggle, start, end)
+    words = map T.unpack $ T.splitOn (T.pack " ") $ T.pack instruction
     readInt = read::String->Int
     comma = T.pack ","
     coord str = map (readInt . T.unpack) $ T.splitOn comma $ T.pack str
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -8,7 +8,7 @@ import Test.QuickCheck.Modifiers
 import Debug.Trace
 
 import Test.Hspec
-import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 import Day6
 
@@ -17,9 +17,9 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  let emptyBoard = Set.empty
-  let light = (10, 10)
-  let board = Set.singleton (10, 10)  
+  let emptyBoard = mkBoard
+  let light = mkLight (10, 10)
+  let board = turnOn light emptyBoard
 
   describe "Turning on lights" $ do
     it ("Turns on " ++ show light) $ do
@@ -29,6 +29,7 @@ spec = do
     context "When the light is on" $ do
       it ("Turns off " ++ show light) $ do
         turnOff light board `shouldSatisfy` isOff light
+
     context "When the light is off" $ do
       it "Still off" $ do
         turnOff light emptyBoard `shouldSatisfy` isOff light
@@ -45,36 +46,56 @@ spec = do
 
 
   describe "Parsing instruction" $  do
-    let coords = [(i,j) | i<-[10..20], j<-[10..20]]
+    let range = [(i,j) | i<-[10..20], j<-[10..20]]
     context "when the instruction is turn on" $ do
       it "turns on the lights in range" $ do
-        let actual = command "turn on 10,10 through 20,20" emptyBoard
-        let lights = map (flip isOn $ actual) coords 
-        and lights `shouldBe` True
+        let actual = lightIt "turn on 10,10 through 20,20" emptyBoard
+        allOn actual range `shouldBe` True
         lightCount actual `shouldBe` 121
 
     context "when the instruction is turn off" $ do
       it "turns off the lights in range" $ do
-        let board = command "turn on 10,10 through 20,20" emptyBoard
-        let actual = command "turn off 10,10 through 20,20" board
-        let lights = map (flip isOff $ actual) coords 
-        and lights `shouldBe` True
+        let board = lightIt "turn on 10,10 through 20,20" emptyBoard
+        let actual = lightIt "turn off 10,10 through 20,20" board
+        allOff actual range `shouldBe` True
         lightCount actual `shouldBe` 0
 
     context "toggle first line" $ do
       it "turns on first line" $ do
-        let actual = command "toggle 0,0 through 999,0" emptyBoard
+        let actual = lightIt "toggle 0,0 through 999,0" emptyBoard
         lightCount actual `shouldBe` 1000
 
     context "Turn off middle" $ do
       it "turns off middle" $ do
-        let board = command "turn on 490,490 through 510,510" emptyBoard
-        let actual = command "turn off 499,499 through 500,500" board
+        let board = lightIt "turn on 490,490 through 510,510" emptyBoard
+        let actual = lightIt "turn off 499,499 through 500,500" board
         lightCount actual `shouldBe` 437
 
+  describe "Increase brightness" $ do
+    it "changes the brightness to 1" $ do
+      let board = incBright 1 (1, 1) $  mkBoard
+      brightQ (1, 1) board `shouldBe` 1
 
---   describe "Reading instructions from file" $ do
---     it "Reads all the instructions" $ do
---       contents <- readFile "test/day6.input.txt"
---       let count = lightCount $ foldl (flip command) emptyBoard $ lines contents
---       count `shouldBe` 400410
+    it "increase the brightness to 5" $ do
+      let board = incBright 5 (1, 1) $ mkBoard
+      brightQ (1, 1) board `shouldBe` 5
+
+    it "increases brightness by 1" $ do
+      let board = brightIt mkBoard "turn on 0,0 through 0,0" 
+      brightness board `shouldBe` 1
+
+    it "increases brightness by 2000000" $ do
+      let board = brightIt mkBoard "toggle 0,0 through 999,999" 
+      brightness board `shouldBe` 2000000
+
+--  describe "Reading instructions from file" $ do
+--    it "Reads all the instructions" $ do
+--      contents <- readFile "test/day6.input.txt"
+--      let count = lightCount $ foldl (flip lightIt) emptyBoard $ lines contents
+--      count `shouldBe` 400410
+--
+--  describe "Checking brightness from file" $ do
+--    it "Reads all the instructions" $ do
+--      contents <- readFile "test/day6.input.txt"
+--      let brt = brightness $ foldl brightIt emptyBoard $ lines contents
+--      brt `shouldBe` 15343601
